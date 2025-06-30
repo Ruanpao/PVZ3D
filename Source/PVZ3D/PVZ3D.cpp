c0115aea192c2d713d43332d733c9e4f514d1820
Ruanpao <ltcdz114514@gmail.com>
2025-06-30 23:07:20 +0800

    upload

--- Source/PVZ3D/PVZ3D.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "PVZ3D.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PVZ3D, "PVZ3D" );
--- Source/PVZ3D/Private/Interface/PVZ3DInteractInterface.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Interface/PVZ3DInteractInterface.h"
-
